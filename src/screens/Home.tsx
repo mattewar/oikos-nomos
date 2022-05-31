7484a1144a3e578c797ad321756f6a77ed85ae60
Matheus Warkentin <matheus.warkentin@brainz.group>
2022-05-31 18:57:09 -0300

    base da home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,38 @@
+import { Col, Container, Row, Card } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import Troia from "../assets/images/troia_bb.webp"
 
 function Home() {
 
     return (
-        <>
-            <Link to="/">
-                <div>home</div></Link>
-
-            <Link to="/realm">
-                <div>reino</div></Link>
-
-        </>
+        <Container>
+            <Row>
+                <Col sm={12} md={4} lg={4} className="mt-4">
+                    <Link className="text-decoration-none text-reset" to="/realm">
+                        <Card>
+                            <Card.Img variant="top" src={Troia} />
+                            <Card.Footer className="text-muted">Tróia 1</Card.Footer>
+                        </Card>
+                    </Link>
+                </Col>
+                <Col sm={12} md={4} lg={4} className="mt-4">
+                    <Link className="text-decoration-none text-reset" to="/realm">
+                        <Card>
+                            <Card.Img variant="top" src={Troia} />
+                            <Card.Footer className="text-muted">Tróia 2</Card.Footer>
+                        </Card>
+                    </Link>
+                </Col>
+                <Col sm={12} md={4} lg={4} className="mt-4">
+                    <Link className="text-decoration-none text-reset" to="/realm">
+                        <Card>
+                            <Card.Img variant="top" src={Troia} />
+                            <Card.Footer className="text-muted">Tróia 3</Card.Footer>
+                        </Card>
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
