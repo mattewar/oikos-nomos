39f398cc02c3eab660977c581d53004d65e5c922
Matheus Warkentin <matheus.warkentin@brainz.group>
2022-05-31 18:41:53 -0300

    Criado header comum

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,12 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './screens/Home';
 import Realm from './screens/Realm';
+import Header from './screens/Header';
 
 function App() {
-  console.log(1);
   return (
     <BrowserRouter>
+    <Header></Header>
       <Routes>
         <Route path="/realm" element={<Realm />} />
         <Route path="/" element={<Home />} />
